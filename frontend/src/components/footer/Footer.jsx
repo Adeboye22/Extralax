c8b2979a23086c82e38bb004f1f8955bd8816ec6
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-10-19 12:53:13 +0100

    other commits

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-green p-8 text-footColor'>
+        <div className='sm:flex sm:flex-col sm:gap-16 sm:p-12 bg-green text-footColor '>
             {/* Location */}
             <div className='pb-4'>      
-                <h1 className='text-xl font-bold pb-2'>Location:</h1>
-                <span className='text-justify text-lg'>We are located at No.1 Ericsson Arubayi close, Golden Kite bus-stop, off Refinery road, Effurun, Warri city, Delta state.</span>
+                <h1 className='text-xl font-bold pb-2'>Office Location:</h1>
+                <span className='text-lg'>We are located at No.1 Ericsson Arubayi close, Golden Kite bus-stop, off Refinery road, Effurun, Warri city, Delta state.</span>
             </div>
             <div>
-                <h1 className='text-xl font-bold pb-2'>hk:</h1>
+                <h1 className='text-xl font-bold pb-2'>Contact Us:</h1>
+                <span className='text-lg'><strong>Telephone numbers:</strong> +234-8054223329; +234-8122856468</span><br/>
+                <span className='text-lg'><strong>Email:</strong> contact@extralax.com.ng</span>
             </div>
         </div>
     )
--- frontend/src/components/home/Homepage.jsx
+++ frontend/src/components/home/Homepage.jsx
@@ -23,12 +23,12 @@ const Homepage = () => {
                         <Hero/>
                     </div>
                 </section>
-                <section data-aos='fade-up' >
-                    <div className='sm:p-3 md:p-4 text-altBlack'>
-                        <h1 className=' sm:pb-6 sm:font-bold md:text-3xl md:pb-6 md:font-bold'>Welcome to Extralax Nigeria Limited</h1>
+                <section data-aos='fade-up' className='sm:px-8'>
+                    <div className='sm:pb-4 text-altBlack'>
+                        <h1 className='sm:pb-8 sm:font-bold md:text-3xl md:pb-8 md:font-bold'>Welcome to Extralax Nigeria Limited</h1>
                         <span className='sm:text-justify md:text-justify sm:text-xl md:text-lg'>EXTRALAX NIGERIA LIMITED is a wholly indigenous oil service company with strong capabilities in Oil Wellhead Maintenance and Wire Line Services. We provide our clients the supplementary resources required to enhance its efforts necessary to undertake a wide range of projects.</span>
                     </div>
-                    <div className='px-3 text-altBlack'>
+                    <div className='text-altBlack'>
                         <button onClick={readAbout} className='sm:p-2 sm:bg-orange sm:border sm:rounded sm:text-sm'>Read More</button>
                     </div>
                 </section>
